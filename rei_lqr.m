186c5e2c82ef7c77a0c80e550e62d2756c2a4eab
Alejandro <alejandrosohe@hotmail.com>
2019-06-16 02:39:33 -0600

    rei_lqr changes
    added function to check controllability

--- rei_lqr.m
+++ rei_lqr.m
@@ -1,11 +1,38 @@
 function [K,Ki] = rei_lqr(plantaMIMO, Q, R)
      
     [A,B,C,D] = ssdata(plantaMIMO);
-     
-     A_aug = [A;C];
-     A_aug_size = size(A_aug);
-     A_aug = [A_aug,zeros(A_aug_size(1),1)];
-     
-     B_aug = [B;zeros(A_aug_size(1),1)];
-     
+
+    A_aug = [A;C];
+    A_aug_size = size(A_aug);
+    A_aug = [A_aug,zeros(A_aug_size(1),1)];
+
+    B_aug = [B;zeros(A_aug_size(1),1)];
+    
+    % Check controllability of the augmented system
+    if (~controlabilidad(A_aug,B_aug))
+        error('Sistema de estados aumentados no es controlable. Ingrese un sistema controlable.')
+    end
+    
+    % ----------------- %
+    % --- FUNCTIONS --- %
+    
+    % Function to check controllability
+    function controlable = controlabilidad(A,B)
+        rows_A = size(A,1);
+        
+        % Compute the Controllability Matrix
+        % ContMat = [B AB A^2B ... A^(n-1)B]
+        ContMat = B;
+        for i=1:1:rows_A-1
+            ContMat = [ContMat, A^i];
+        end
+        
+        % Check Controllability with the matrix rank
+        if(rank(ContMat) == rows_A)
+            controlable = true;
+        else
+            controlable = false;
+        end
+    end
+
 end 
