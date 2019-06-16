4e5ff4cd299d13cba6a16487cc26822a329c1eea
Alejandro <alejandrosohe@hotmail.com>
2019-06-16 03:21:30 -0600

    rei_lqr
    error in line 9 was commented, and the controllability test was tested with 5 examples

--- rei_lqr.m
+++ rei_lqr.m
@@ -1,18 +1,20 @@
 function [K,Ki] = rei_lqr(plantaMIMO, Q, R)
      
     [A,B,C,D] = ssdata(plantaMIMO);
-
+        
     A_aug = [A;C];
     A_aug_size = size(A_aug);
     A_aug = [A_aug,zeros(A_aug_size(1),1)];
 
-    B_aug = [B;zeros(A_aug_size(1),1)];
+    %B_aug = [B;zeros(A_aug_size(1),1)];
     
     % Check controllability of the augmented system
-    if (~controlabilidad(A_aug,B_aug))
+    if (~controlabilidad(A,B))
         error('Sistema de estados aumentados no es controlable. Ingrese un sistema controlable.')
+    else
+        disp("Controlable :)")
     end
-    
+
     % ----------------- %
     % --- FUNCTIONS --- %
     
@@ -24,7 +26,7 @@ function [K,Ki] = rei_lqr(plantaMIMO, Q, R)
         % ContMat = [B AB A^2B ... A^(n-1)B]
         ContMat = B;
         for i=1:1:rows_A-1
-            ContMat = [ContMat, A^i];
+            ContMat = [ContMat, mpower(A,i)*B];
         end
         
         % Check Controllability with the matrix rank
