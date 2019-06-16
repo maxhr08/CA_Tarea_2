5a52a1ecabd926d125a960d1b55adec8b6aa74c5
Max Hronek <30559609+maxhr08@users.noreply.github.com>
2019-06-15 19:58:41 -0600

    Update rei_lqr.m

--- rei_lqr.m
+++ rei_lqr.m
@@ -1,4 +1,11 @@
 function [K,Ki] = rei_lqr(plantaMIMO, Q, R)
-
-
-end
+     
+    [A,B,C,D] = ssdata(plantaMIMO);
+     
+     A_aug = [A;C];
+     A_aug_size = size(A_aug);
+     A_aug = [A_aug,zeros(A_aug_size(1),1)];
+     
+     B_aug = [B;zeros(A_aug_size(1),1)];
+     
+end 
